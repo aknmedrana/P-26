0c042418c94cd9ba05f2c2f7c8c3433ac5cd7402
Anne Kathlyn <annekathlynmedrana03@gmail.com>
2026-07-04 19:50:34 +0800

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/P-26/",
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
